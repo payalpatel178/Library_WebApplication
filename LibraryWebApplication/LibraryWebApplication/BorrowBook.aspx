e9cedc340129ea75c432e0bdc80cb2a146e83b68
Payal Patel <patelpayal1796@gmail.com>
2020-04-26 23:01:26 -0400

    Adding page from where book can be borrowed

---
+++ LibraryWebApplication/LibraryWebApplication/BorrowBook.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="BorrowBook.aspx.cs" Inherits="LibraryWebApplication.BorrowBook" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Borrow Book</title>
+    <link rel="stylesheet" href="css/style.css"/>
+</head>
+<body>
+    <div class="topnav">
+  <a href="https://localhost:44333/index.aspx">Home</a>
+  <a href="https://localhost:44333/AddStudent.aspx">Add Student</a>
+  <a href="https://localhost:44333/AddBook.aspx">Add Book</a>
+  <a class="active" href="#">Borrow Book</a>
+  <a href="https://localhost:44333/ReturnBook.aspx">Return Book</a>
+</div>
+    <form id="form1" runat="server" class="allForms">
+        <div class="heading">
+            BORROW BOOK
+        </div>
+        
+        <p>
+            Select&nbsp; Student</p>
+        <asp:DropDownList ID="ddlSelectStudent" runat="server" AppendDataBoundItems="True" Width="352px" Height="30px">
+            <asp:ListItem Value="0">-- Please Select Student -- </asp:ListItem>
+        </asp:DropDownList>
+        <p>
+            Choose Book</p>
+        <p>
+            <asp:DropDownList ID="ddlchooseBook" runat="server" AppendDataBoundItems="True" Width="350px" Height="30px" style="margin-top: 0px">
+                <asp:ListItem Value="0">-- Please Select Book -- </asp:ListItem>
+            </asp:DropDownList>
+        </p>
+        <asp:Button ID="btnBorrowBook" runat="server" OnClick="Button3_Click" Text="BORROW BOOK" Width="187px" BackColor="#0066FF" BorderColor="#0099FF" BorderStyle="Inset" Font-Bold="True" ForeColor="White" style="margin-left: 78px" Height="38px" />
+        <asp:Label ID="Label3" runat="server"></asp:Label>
+    </form>
+    <div class="footer">
+        <p>All Rights reserved &copy; 2019</p>
+    </div>
+</body>
+</html>
---
+++ LibraryWebApplication/LibraryWebApplication/BorrowBook.aspx.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LibraryWebApplication
+{
+    public partial class BorrowBook : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!this.Page.IsPostBack)
+            {
+                DataTable student = ((DataSet)BusinessLayer.UtilityTools.ExecuteStudentAndBookSelect()).Tables[0];
+
+                DataTable book = ((DataSet)BusinessLayer.UtilityTools.ExecuteStudentAndBookSelect()).Tables[1];
+
+                this.ddlSelectStudent.DataSource = student;
+                this.ddlSelectStudent.DataTextField = student.Columns["StudentName"].ToString();
+                this.ddlSelectStudent.DataValueField = student.Columns[0].ToString();
+
+                this.ddlSelectStudent.DataBind();
+
+                this.ddlchooseBook.DataSource = book;
+                this.ddlchooseBook.DataTextField = book.Columns["BookInfo"].ToString();
+                this.ddlchooseBook.DataValueField = book.Columns[0].ToString();
+
+                this.ddlchooseBook.DataBind();
+            }
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            if (this.Page.IsValid)
+            {
+                BusinessLayer.Borrow borrow = new BusinessLayer.Borrow();
+                borrow.StudentId = Convert.ToInt32(ddlSelectStudent.SelectedItem.Value);
+                borrow.BookId = Convert.ToInt32(ddlchooseBook.SelectedItem.Value);
+                
+                int result = BusinessLayer.UtilityTools.ExecuteBorrowBookInsert(borrow);
+                if (result == 3)
+                {
+                    this.Label3.Text = "Please Choose Valid Book!";
+                }
+                else if (result == 4)
+                {
+                    this.Label3.Text = "Please Choose Valid Student!";
+                }
+                else if (result == 1)
+                {
+                    this.Label3.Text = "Successfully Book Borrowed!";
+                }
+                else if (result == 2)
+                {
+                    this.Label3.Text = "Already Book Borrowed By Another Student!";
+                }
+                else if (result == 0)
+                {
+                    this.Label3.Text = "There was an error at the database level";
+                }
+                else
+                {
+                    Response.Write(result);
+                    this.Label3.Text = "There was an error at the method level";
+                }
+
+            }
+            else
+            {
+                this.Label3.Text = "The page is not valid!";
+            }
+        }
+    }
+}
