756decfc2893882968d5211e7d969da3c9309588
Payal Patel <patelpayal1796@gmail.com>
2020-04-26 23:01:54 -0400

    Adding page from where borrowed book can be returned

---
+++ LibraryWebApplication/LibraryWebApplication/ReturnBook.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ReturnBook.aspx.cs" Inherits="LibraryWebApplication.ReturnBook" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Return Book</title>
+    <link rel="stylesheet" href="css/style.css"/>
+</head>
+<body>
+    <div class="topnav">
+  <a href="https://localhost:44333/index.aspx">Home</a>
+  <a href="https://localhost:44333/AddStudent.aspx">Add Student</a>
+  <a href="https://localhost:44333/AddBook.aspx">Add Book</a>
+  <a href="https://localhost:44333/BorrowBook.aspx">Borrow Book</a>
+  <a class="active" href="#">Return Book</a>
+</div>
+    <form id="form1" runat="server" class="allForms">
+        <div class="heading">
+            RETURN BOOK</div>
+        <p>
+            Select Student</p>
+        <p>
+            <asp:DropDownList ID="ddlSelectStudentToReturnBook" runat="server" AppendDataBoundItems="True" Width="285px" OnSelectedIndexChanged="ddlSelectStudentToReturnBook_SelectedIndexChanged" AutoPostBack="true" Height="30px">
+            <asp:ListItem Value="0">-- Please Select student -- </asp:ListItem>
+            </asp:DropDownList>
+        </p>
+        <p>
+            Select Book To Be Returned by Student</p>
+        <asp:DropDownList ID="ddlChooseBookToReturn" AppendDataBoundItems="True" runat="server" Width="285px" Height="30px">
+            <asp:ListItem Value="0">-- List Of Borrowed Books -- </asp:ListItem>
+        </asp:DropDownList>
+        <p>
+            <asp:Button ID="btnReturnBook" runat="server" BackColor="#0066FF" BorderColor="#0099FF" BorderStyle="Inset" Font-Bold="True" ForeColor="White" Height="42px" style="margin-left: 39px" Text="RETURN BOOK" Width="190px" OnClick="Button4_Click" />
+            <asp:Label ID="Label4" runat="server"></asp:Label>
+        </p>
+    </form>
+    <div class="footer">
+        <p>All Rights reserved &copy; 2019</p>
+    </div>
+</body>
+</html>
---
+++ LibraryWebApplication/LibraryWebApplication/ReturnBook.aspx.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LibraryWebApplication
+{
+    public partial class ReturnBook : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!this.Page.IsPostBack)
+            {
+
+                DataTable student = ((DataSet)BusinessLayer.UtilityTools.ExecuteStudentWithBookReturnSelect()).Tables[0];
+
+                this.ddlSelectStudentToReturnBook.DataSource = student;
+                this.ddlSelectStudentToReturnBook.DataTextField = student.Columns["StudentName"].ToString();
+                this.ddlSelectStudentToReturnBook.DataValueField = student.Columns[0].ToString();
+
+                this.ddlSelectStudentToReturnBook.DataBind();
+
+            }
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            if (this.Page.IsValid)
+            {
+                
+                BusinessLayer.Borrow returnBook = new BusinessLayer.Borrow();
+                returnBook.StudentId = Convert.ToInt32(ddlSelectStudentToReturnBook.SelectedItem.Value);
+                returnBook.BookId = Convert.ToInt32(ddlChooseBookToReturn.SelectedItem.Value);
+
+                int result = BusinessLayer.UtilityTools.ExecuteReturnBookUpdateBroughtDate(returnBook);
+                if (result == 3)
+                {
+                    this.Label4.Text = "Please Choose Valid Student!";
+                }
+                else if (result == 1)
+                {
+                    this.Label4.Text = "Successful Book Returned!";
+                }
+                else if (result == 0)
+                {
+                    this.Label4.Text = "There was an error at the database level";
+                }
+                else if (result == 2)
+                {
+                    this.Label4.Text = "Please choose Valid Book!";
+                }
+                else
+                {
+                    Response.Write(result);
+                    this.Label4.Text = "There was an error at the method level";
+                }
+
+            }
+            else
+            {
+                this.Label4.Text = "The page is not valid!";
+            }
+        }
+
+        protected void ddlSelectStudentToReturnBook_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ddlChooseBookToReturn.Enabled = false;
+            ddlChooseBookToReturn.Items.Clear();
+            ddlChooseBookToReturn.Items.Insert(0, new ListItem("Select Book", "0"));
+
+            int studentId = Convert.ToInt32(ddlSelectStudentToReturnBook.SelectedItem.Value);
+            //int studentId = 4;
+
+
+            if (studentId > 0)
+            {
+                string query = string.Format("SELECT  B.bookId AS bookId,B.title + ' - ' + T.[name]  + ' - ' + A.firstname + '  ' + A.lastname AS BookInfo FROM dbo.book B INNER JOIN dbo.type T ON B.typeId = T.typeId INNER JOIN dbo.author A ON A.authorId = B.authorId INNER JOIN dbo.borrow BR ON BR.bookId = B.bookId WHERE BR.studentId = {0} AND BR.broughtDate IS NULL", studentId);
+        
+                BindDropDownList(ddlChooseBookToReturn, query, "bookInfo", "bookId", "Select Book");
+                ddlChooseBookToReturn.Enabled = true;
+            }
+                
+            
+        }
+
+        private void BindDropDownList(DropDownList ddl, string query, string text, string value, string defaultText)
+        {
+            string conString = "Data Source=.;Initial Catalog=LibraryWebDB;Integrated Security=True";
+            SqlCommand cmd = new SqlCommand(query);
+            using (SqlConnection con = new SqlConnection(conString))
+            {
+                using (SqlDataAdapter sda = new SqlDataAdapter())
+                {
+                    cmd.Connection = con;
+                    con.Open();
+                    ddl.DataSource = cmd.ExecuteReader();
+                    ddl.DataTextField = text;
+                    ddl.DataValueField = value;
+                    ddl.DataBind();
+                    con.Close();
+                }
+            }
+            //ddl.Items.Insert(0, new ListItem(defaultText, "0"));
+        }
+
+    }
+}
